620c1113b8e6c9389fea29376b3496152f091218
Yoann <ychappaz.ir2021@esaip.org>
2019-06-03 15:44:27 +0200

    Improve prog

--- finalProject/cppProgram/main.cpp
+++ finalProject/cppProgram/main.cpp
@@ -6,10 +6,18 @@ int main(int argc, char *argv[])
 
 	mosqpp::lib_init();
     
-    if (argc == 2) clientMqtt = new mqtt("RPi", argv[1], 1883);
-    if (argc == 3) clientMqtt = new mqtt("RPi", argv[1], atoi(argv[2]));
-	else clientMqtt = new mqtt("RPi", "localhost", 1883);
-    
+	if (argc == 2){
+	  printf("Try to connect in %s on 1883!\n", argv[1]);
+	  clientMqtt = new mqtt("RPi", argv[1], 1883);
+	}
+	else if (argc == 3){
+	  printf("Try to connect in %s on %d !\n", argv[1], atoi(argv[2]));
+	  clientMqtt = new mqtt("RPi", argv[1], atoi(argv[2]));
+	}
+	else {
+	  printf("Try to connect in localhost !\n");
+	  clientMqtt = new mqtt("RPi", "localhost", 1883);
+	}
 	clientMqtt->loop_forever();
 
 	mosqpp::lib_cleanup();
